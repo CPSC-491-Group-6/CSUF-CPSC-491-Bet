2c9733ec76260ad940b892c4fcdcd0194d879905
Lam Doan <lamdoan@Lams-MacBook-Air.local>
2026-09-21 17:28:56 -0700

    Start Sprint 2 auth context and session setup

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,73 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+// Who is currently logged in?
+
+const AuthContext = createContext(null)
+
+export function AuthProvide({children}) {
+    // strore current logged-in user
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        checkSession()
+    }, [])
+
+/*
+    async fucntion checkSession() {
+        try {
+            const response = await fetch('/api/auth/me', {
+                credentials: 'include',
+            })
+
+            if (!response.ok) {
+                setUser(null)
+                return
+            }
+
+            const data = await response.json)
+            setUser(data.user)
+        } catch(error) {
+            console.error('Failed to check authentication session:', error)
+            setUser(null)
+        } finally {
+            setLoading(false)
+        }
+    }
+*/
+    
+    // waiting for member C's authentication endpoints
+    async function checkSession() {
+        try {
+            setUser(null)
+        } catch (error) {
+            console.error('Failed to check authentication session:', error)
+            setUser(null)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    function login(userData) {
+        setUser(userData)
+    }
+
+    function logout() {
+        setUser(null)
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                loading,
+                isAuthenticated: Boolean(user),
+                login,
+                logout,
+                checkSession
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
